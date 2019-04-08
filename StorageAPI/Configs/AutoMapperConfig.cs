8a559c64ef3382099999a7bf435b65a5158bd3b8
Viktor Sergijenko <viktor.sergijenko@bbit.tech>
2019-04-08 19:48:32 +0300

    Added Mapping register class

---
+++ StorageAPI/Configs/AutoMapperConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using StorageAPI.Configs.Profiles;
+
+namespace StorageAPI.Configs
+{
+    public class AutoMapperConfig
+    {
+        public static void RegisterMappings(IServiceProvider services)
+        {
+            Mapper.Initialize(c =>
+            {
+                // This is needed for automapper to be able create models
+                // with injectable services
+                c.AddProfile<WarehouseProfile>();
+
+            });
+        }
+    }
+}
