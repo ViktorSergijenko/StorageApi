2b46011cc0ecb4c7c3c5c90d3dbeda98688274a3
Viktor Sergijenko <viktor.sergijenko@bbit.tech>
2019-04-08 19:50:04 +0300

    Added class that will be related with sorting methods

---
+++ StorageAPI/Models/FilterSorting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StorageAPI.Models
+{
+    public class FilterSorting
+    {
+        public string FilterOption { get; set; }
+    }
+}
