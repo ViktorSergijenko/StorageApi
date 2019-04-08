adee48311a933a734a3df0c768276cc86324acfa
Viktor Sergijenko <viktor.sergijenko@bbit.tech>
2019-04-08 19:47:44 +0300

    Modified namespace

---
+++ StorageAPI/Configs/Profiles/CatalogProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using StorageAPI.Models;
+
+namespace StorageAPI.Configs.Profiles
+{
+    public class CatalogProfile : Profile
+    {
+        CatalogProfile() {
+            CreateMap<Catalog, Catalog>();
+
+        }
+    }
+}
---
+++ StorageAPI/Configs/Profiles/WarehouseProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StorageAPI.Models;
+using AutoMapper;
+
+namespace StorageAPI.Configs.Profiles
+{
+    public class WarehouseProfile : Profile
+    {
+        public WarehouseProfile()
+        {
+            CreateMap<Warehouse, Warehouse>()
+                .ForMember(x => x.QrCodeBase64, o => o.Ignore());
+        }
+    }
+}
