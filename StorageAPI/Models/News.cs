719ed02fd96661544f5ba6719baa0398d15e1bbc
Viktor Sergijenko <viktor.sergijenko@bbit.tech>
2019-04-13 11:18:51 +0300

    Added News model

---
+++ StorageAPI/Models/News.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StorageAPI.Models
+{
+    public class News : BaseEntity
+    {
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public Guid WarehouseId { get; set; }
+        public Warehouse Warehouse { get; set; }
+        public bool FixedProblem { get; set; }
+
+
+    }
+}
