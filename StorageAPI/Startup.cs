f9e2466c88977cc6a664b9eb8db71a2b3c0a19a6
Viktor Sergijenko <viktor.sergijenko@bbit.tech>
2019-04-13 11:21:25 +0300

    Roled back all cores and sql server connection string to development settings

--- StorageAPI/Startup.cs
+++ StorageAPI/Startup.cs
@@ -30,16 +30,18 @@ namespace StorageAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<StorageContext>(opt => opt.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=WarehouseStorage;Trusted_Connection=True;"));
+            //"Server=tcp:warehouse-manager-dbserver.database.windows.net,1433;Initial Catalog=warehouse-manager-api_db;Persist Security Info=False;User ID=viktor@warehouse-manager-dbserver;Password=bRAVO1996;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+            services.AddDbContext<StorageContext>(
+                opt => opt.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=WarehouseStorage;Trusted_Connection=True;"));
             services.AddAutoMapper(typeof(Startup).Assembly);
             services.AddMvc()
                 .AddJsonOptions(o => o.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors(options =>
             {
-                options.AddPolicy("AllowAnyOrigin",
+                options.AddPolicy("AllowOneOrigin",
                     builder => builder
-                    .AllowAnyOrigin()
+                    .WithOrigins("http://localhost:4200")
                     .AllowAnyMethod()
                     .AllowAnyHeader()
                     );
@@ -50,6 +52,7 @@ namespace StorageAPI
             services.AddSingleton<IConfiguration>(o => Configuration);
             services.AddScoped<StorageContext>();
             services.AddScoped<WarehouseService>();
+            services.AddScoped<NewsService>();
             AutoMapperConfig.RegisterMappings(services.BuildServiceProvider());
             #endregion Services injections
 
@@ -70,7 +73,7 @@ namespace StorageAPI
 
             app.UseHttpsRedirection();
             app.UseMvc();
-            app.UseCors("AllowAnyOrigin");
+            app.UseCors("AllowOneOrigin");
         }
     }
 }
